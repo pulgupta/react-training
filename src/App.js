26ae6cf99795fe428c33a01f0831b34298f94a2d
Pulkit Gupta <pulkit.gupta1@Pulkits-MacBook-Pro.local>
2019-07-30 11:49:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// A very small component
+const Img = props => {
+  return <img src={logo} className="App-logo" alt={props.alt} />;
+};
+
+// this means that I was getting a props but now react will just pick two properties from the props and pass to our method.
+const Img1 = ({ alt: alt, className }) => {
+  return <img src={logo} className="App-logo" alt={alt} />;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Img alt="xyz" />
+        <Img1 alt="abc" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
