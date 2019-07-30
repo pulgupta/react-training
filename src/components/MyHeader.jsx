62bd17541647a17a81a392d87ffe20bd4b26baab
Pulkit Gupta <pulkit.gupta1@Pulkits-MacBook-Pro.local>
2019-07-30 15:02:39 +0530

    added proptype for children

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const MyHeader = ({ children }) => {
   return <header>{children}</header>;
+};
+
+MyHeader.propTypes = {
+  children: PropTypes.array
 };
 
 export default MyHeader;
