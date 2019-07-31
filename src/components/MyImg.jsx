c527dfe56a56d1ba1ce72895ddc4b6794abecef7
Pulkit Gupta <pulkit.gupta1@Pulkits-MacBook-Pro.local>
2019-07-31 11:49:44 +0530

    added more tests

--- src/components/MyImg.jsx
+++ src/components/MyImg.jsx
@@ -4,7 +4,11 @@ import PropTypes from "prop-types";
 
 // just copied the same thing from app.js so that we can create an actual component
 const MyImg = props => {
-  return <img src={logo} className="App-logo" alt={props.alt} />;
+  if (!props.alt) {
+    throw "sasa";
+  }
+  let alt1 = "hello-" + props.alt;
+  return <img src={logo} className="App-logo" alt={alt1} />;
 };
 
 MyImg.propTypes = {
--- src/components/__test__/Image.test.js
+++ src/components/__test__/Image.test.js
@@ -1,16 +1,21 @@
 import MyImg from "../MyImg";
-// Because we are doing unit test we will just do a shallow which will not create the child components
+// Because we are doing unit test, we will just do a shallow which will not create any child components
 import { shallow } from "enzyme";
 import React from "react";
 
 describe("Img Details", () => {
   it("should render without crashing", () => {
-    shallow(<MyImg />);
+    shallow(<MyImg alt="asda" />);
   });
 
   it("should have alt property", () => {
     const wrapper = shallow(<MyImg alt={"text"} className={"App-logo"} />);
     const img = wrapper.find(".App-logo"); // . css selector
-    expect(img.props().alt).toBe("text"); // Now check if are receiving the same prop
+    expect(img.props().alt).toBe("hello-text"); // Now check if are receiving the same prop
+  });
+
+  it("should have the correct source", () => {
+    const wrapper = shallow(<MyImg alt={"text"} className={"App-logo"} />);
+    const img = wrapper.find(".App-logo");
   });
 });
